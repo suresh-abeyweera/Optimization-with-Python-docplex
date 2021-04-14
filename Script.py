c7b180096a198bd9decf42c81b2965d09e95f72c
suresh-abeyweera <61721484+suresh-abeyweera@users.noreply.github.com>
2021-04-14 11:45:08 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -6,17 +6,17 @@ CarA = my_model.integer_var(name='CarA')
 CarB = my_model.integer_var(name='CarB')
 
 
-# constraint #1: CarA production is greater than 100
+# constraint #1: CarA production is greater than 10
 my_model.add_constraint(CarA >= 10)
 
-# constraint #2: CarB production is greater than 100
+# constraint #2: CarB production is greater than 10
 my_model.add_constraint(CarB >= 10)
 
 # constraint #3: Assembly Line has a Maximum Capacity Limitaion
 ct_assembly = my_model.add_constraint( 0.5 * CarA + 0.25 * CarB <= 20)
 
 # constraint #4: Painting and Finishing Line has a Maximum Capacity Limitation.
-ct_painting = my_model.add_constraint( 0.5 * CarA + 0.1 * CarB <= 10)
+ct_painting = my_model.add_constraint( 0.15 * CarA + 0.1 * CarB <= 10)
 
 
 my_model.maximize(12000 * CarA + 15000 * CarB)
